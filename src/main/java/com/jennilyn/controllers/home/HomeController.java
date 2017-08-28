37d970f852d660e2ccf1ada81ba8717a6a143f5a
Jennilyn Howell <jennilynhowell@me.com>
2017-08-28 15:14:27 -0400

    able to add new user w pw saved as hash

--- src/main/java/com/jennilyn/controllers/home/HomeController.java
+++ src/main/java/com/jennilyn/controllers/home/HomeController.java
@@ -1,13 +1,18 @@
 package com.jennilyn.controllers.home;
 
+import com.jennilyn.models.Role;
 import com.jennilyn.models.User;
 import com.jennilyn.repositories.ProductRepository;
+import com.jennilyn.repositories.RoleRepository;
 import com.jennilyn.repositories.UserRepository;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,10 +20,13 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
 
     @Autowired
-    ProductRepository productRepo;
+    RoleRepository roleRepo;
 
     @Autowired
-    UserRepository userRepository;
+    UserRepository userRepo;
+
+    @Autowired
+    BCryptPasswordEncoder encoder;
 
     @RequestMapping("/")
     public String index(){
@@ -37,6 +45,31 @@ public class HomeController {
         } catch (Exception ex) {}
 
         return "login";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signup(@RequestParam("username") String username,
+                         @RequestParam("password") String password,
+                         @RequestParam("email") String email,
+                         @RequestParam("first") String first,
+                         @RequestParam("last") String last){
+        User newUser = new User();
+        Role userRole = roleRepo.findByName("ROLE_USER");
+        newUser.setUsername(username);
+        newUser.setPassword(encoder.encode(password));
+        newUser.setEmail(email);
+        newUser.setFirst(first);
+        newUser.setLast(last);
+        newUser.setRole(userRole);
+        newUser.setActive(true);
+        userRepo.save(newUser);
+        return "redirect:/login";
     }
 
 }
